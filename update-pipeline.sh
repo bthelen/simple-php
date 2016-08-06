6dbffe5e8806e63577071a786425b907b99934a4
mborges-pivotal <mborges@gopivotal.com>
2016-08-06 01:24:07 -0500

    remove aws credentials

--- update-pipeline.sh
+++
@@ -1,6 +0,0 @@
-###################
-# install/update pipeline command
-###################
-
-# Load properties file, but pass all credentials during the install/update of the pipeline
-fly set-pipeline -p ge-calc-production -c production-pipeline.yml -v "repo-private-key=$(cat id_rsa)" -v "cf-password=pivotal" -v "AWS-key-id=AKIAJGQW3IS4FT5RYFEQ" -v "AWS-secret=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" -l properties.yml
