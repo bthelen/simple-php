7eb94789937cdd4957e2f92eedb2836637d852e9
mborges-pivotal <mborges@gopivotal.com>
2016-08-06 19:59:49 -0500

    ci fixes

--- ci/prepare-manifest.sh
+++ ci/prepare-manifest.sh
@@ -10,18 +10,25 @@ cp release/$CF_ARTIFACT_ID-*.tar.gz prepare-manifest-output/.
 pushd prepare-manifest-output
 
 ARTIFACT_PATH=$(ls $CF_ARTIFACT_ID-*.tar.gz)
+echo "########### $ARTIFACT_PATH #############"
+
+tar -xzvf $ARTIFACT_PATH 
 
 cat <<EOF >manifest.yml
 ---
 applications:
 - name: $CF_APP_NAME
   host: $CF_APP_HOST
-  path: $ARTIFACT_PATH
+  path: ./project
   memory: 256M
   instances: 1
   timeout: 180
   services: [ $CF_APP_SERVICES ]
+  buildpack: php_oracle_buildpack
   env:
     BP_DEBUG: "True"
 EOF
+
+cat manifest.yml
+
 popd
