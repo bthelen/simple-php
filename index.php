7ddbbc343729da99d1997da81dd842a05d2d43b9
mborges-pivotal <mborges@gopivotal.com>
2017-01-19 22:39:18 -0600

    exxon demo 1

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ use Stomp\Client;
 use Stomp\Exception\StompException;
 use Stomp\Stomp;
 
-echo "<h1>TEST 5 - GM</h1>";
+echo "<h1>TEST 1 - Exxon</h1>";
 
 /////////////////////////////////////////////////////////
 // CF
